c73efa83ff1e866190616edd87488bfd1502f5b3
Robert Lemke <robert@typo3.org>
2008-03-31 15:34:21 +0000

    Added an "index_dev.php" which runs FLOW3 in development context.

---
+++ Public/index_dev.php
@@ -0,0 +1,32 @@
+<?php
+declare(encoding = 'utf-8');
+
+/*                                                                        *
+ * This script is part of the TYPO3 project - inspiring people to share!  *
+ *                                                                        *
+ * TYPO3 is free software; you can redistribute it and/or modify it under *
+ * the terms of the GNU General Public License version 2 as published by  *
+ * the Free Software Foundation.                                          *
+ *                                                                        *
+ * This script is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
+ * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General      *
+ * Public License for more details.                                       *
+ *                                                                        */
+
+/**
+ * Bootstrap for the FLOW3 Framework in development context
+ *
+ * @package FLOW3
+ * @version $Id$
+ * @author Robert Lemke <robert@typo3.org>
+ * @copyright Copyright belongs to the respective authors
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License, version 2
+ */
+
+define('FLOW3_PATH_PUBLIC', str_replace('\\', '/', dirname(__FILE__)) . '/');
+require_once(FLOW3_PATH_PUBLIC . '../Packages/FLOW3/Classes/F3_FLOW3.php');
+
+$framework = new F3_FLOW3('Development');
+$framework->run();
+?>
