483011b9b158a407cc4b540dadb7442d19232003
Robert Lemke <robert@typo3.org>
2008-07-30 13:05:26 +0000

    * Global Configuration: Moved the default routes configuration to within the FLOW3 package * FLOW3: (Configuration) Refactored the configuration manager. Now there's a dedicated method getSettings() for retrieving settings for a certain package. You will most likely want to use this method instead of the now called "getSpecialConfiguration()" method. * FLOW3: (Configuration) Converted the configuration source into a non-static class. * FLOW3: (Configuration) Implemented proper tests for the Configuration Manager * TYPO3CR and TYPO3: Adapted the code to use the new configuration manager methods * FLOW3: The special "FLOW3 configuration" (as defined in FLOW3.php) technically are now just settings for the package "FLOW3" with the only difference that they are loaded earlier than other settings and can only be set in the FLOW3 package and the global configuration. * FLOW3: (MVC) Refactored the Routing mechanism - mainly cosmetic changes. Among other things removed the Route Interface. * FLOW3: (MVC) Refactored the Web Request Builder a bit. * FLOW3: (Security) Fixed the Context Holder Session destructor which was throwing uncatched exceptions while trying to close an uninitalized session. Needs further cleanup / handling.

--- Configuration/Routes.php
+++ Configuration/Routes.php
@@ -10,29 +10,4 @@ declare(ENCODING="utf-8");
  * Please refer to the FLOW3 manual for possible configuration options.   *
  *                                                                        */
 
-/**
- * Default route to map the first three URL segments to package, controller and action
- */
-$c->routes->default
-	->setUrlPattern('[package]/[controller]/[action]')
-	->setDefaults(
-		array(
-			'package' => 'Default',
-			'controller' => 'Default',
-			'action' => 'Default',
-		)
-	);
-
-/**
- * Example route which acts as a shortcut to /TYPO3/Page/Default
- */
-$c->routes->typo3
-	->setUrlPattern('typo3')
-	->setDefaults(
-		array(
-			'package' => 'TYPO3',
-			'controller' => 'Page',
-			'action' => 'Default',
-		)
-	);
 ?>
