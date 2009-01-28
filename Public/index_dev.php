f588de93384f054cf5d78bac067a7f6fd3435e86
Robert Lemke <robert@typo3.org>
2009-01-28 11:44:08 +0000

    Adapted the index*.php files to match the new namespace of the FLOW3 bootstrap. Addresses #2524

--- Public/index_dev.php
+++ Public/index_dev.php
@@ -31,9 +31,9 @@ declare(ENCODING = 'utf-8');
  */
 
 define('FLOW3_PATH_PUBLIC', str_replace('\\', '/', __DIR__) . '/');
-require(FLOW3_PATH_PUBLIC . '../Packages/FLOW3/Classes/F3_FLOW3.php');
+require(FLOW3_PATH_PUBLIC . '../Packages/FLOW3/Classes/FLOW3.php');
 
-$framework = new F3\FLOW3('Development');
+$framework = new F3\FLOW3\FLOW3('Development');
 $framework->initialize();
 $framework->run();
 ?>
