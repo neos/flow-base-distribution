fe3db9c15208d82cf19c9c3a404c7b87e366ef53
Karsten Dambekalns <karsten@typo3.org>
2009-05-29 13:18:57 +0000

    FLOW3 distribution: * setfilepermissions.sh now ignores .svn for chown (thanks Tamas Ilsinszki for the hint)

--- setfilepermissions.sh
+++ setfilepermissions.sh
@@ -24,7 +24,7 @@ WEBSERVER_GROUP="$2"
 find . -type d -exec chmod 755 {} \;
 find . -type f -exec chmod 664 {} \;
 
-chown -R $WEBSERVER_USER:$WEBSERVER_GROUP Data/
+find Data -not -regex '.*[.]svn.*' -exec chown $WEBSERVER_USER:$WEBSERVER_GROUP {} \;
 chmod 775 Data
 find Data -type d -exec chmod 775 {} \;
 find Data -type f -exec chmod 664 {} \;
@@ -35,6 +35,5 @@ chmod 755 $0
 chmod 755 Public
 chmod 755 Public/index.php
 
-chown -R $WEBSERVER_USER:$WEBSERVER_GROUP Public/Resources/
 find Public/Resources -type d -exec chmod 775 {} \;
 find Public/Resources -type f -exec chmod 664 {} \;
